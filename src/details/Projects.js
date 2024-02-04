4781a6ff35ca31681dc14e77286ae0843af4415b
GunPKate <gunPK>
2024-02-04 17:32:11 +0700

    refactor

---
+++ src/details/Projects.js
@@ -0,0 +1,30 @@
+export const  Projects = [
+    {
+        name: "A",
+        tag: "node js",
+        tech: ["nodeJS","nodemailer","Hapi"],
+        desc: "use nodemailer to generate",
+        img: "https://picsum.photos/500/500?image=0"
+    },
+    {
+        name: "B",
+        tag: "",
+        tech: ["Java", "Springboot"],
+        desc: "use nodemailer to generate",
+        img: "https://picsum.photos/200/300?image=1"
+    },
+    {
+        name: "C",
+        tag: "REST API",
+        tech: ["C#,Web API",""],
+        desc: "use nodemailer to generate",
+        img: "https://picsum.photos/200/300?image=1"
+    },
+    {
+        name: "D",
+        tag: "C#",
+        tech: "C# ",
+        desc: "use nodemailer to generate",
+        img: "https://picsum.photos/200/300?image=1"
+    },
+  ]
---
+++ src/details/WorkExperience.js
@@ -0,0 +1,23 @@
+export const WorkExperience = [
+    {
+      name: "Advanced Info System",
+      tag: "Software Developer",
+      tech: ["Java", "Springboot","Angular"],
+      desc:"ERP of sale department",
+      img: "https://picsum.photos/200/300"
+    },
+    {
+      name: "Builk One",
+      tag: "Software Developer",
+      tech: ["C#", "Dot net MVC","Angular"],
+      desc:"Construction Software Company",
+      img: "https://picsum.photos/200/300"
+    },
+    {
+      name: "KPMG",
+      tag: "Audit Assistant",
+      tech: "",
+      desc:"Finanical",
+      img: "https://picsum.photos/200/300"
+    }
+  ]
